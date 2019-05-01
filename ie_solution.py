b3fa54c5408956625407e90561bafd54789c790b
William Teder <wteder@hydreon.com>
2019-05-01 00:07:28 -0500

    chore(code-quality): imports, scope, variable lifting

--- ie_solution.py
+++ ie_solution.py
@@ -1,11 +1,9 @@
 # Pulls noun phrases, selects answer among those, reconstructs in original context
-from __future__ import absolute_import, division, print_function
-from random import random, sample, randrange
+from random import sample
 from functools import reduce
 import os
 from nltk.parse.corenlp import CoreNLPServer, CoreNLPParser
 from requests.exceptions import HTTPError
-from pprint import pprint
 import itertools
 import requests
 
@@ -14,32 +12,40 @@ import requests
 # import numpy as np
 # import matplotlib.pyplot as plt
 
-parser = None
 config = {
     'isManagingServer': False,
     'debugSentenceElimination': False,
     'coreNLPServerURL': 'http://localhost:9000'
 }
+parser = CoreNLPParser(url=config['coreNLPServerURL'])
+server = None
 STANFORD = os.path.join("models", "stanford-corenlp-full-2018-10-05")
-server = CoreNLPServer(
-   os.path.join(STANFORD, "stanford-corenlp-3.9.2.jar"),
-   os.path.join(STANFORD, "stanford-corenlp-3.9.2-models.jar"),
-)
+contextCache = {}
 
-def setup(manageServerInternally):
+def setup(manageServerInternally=False):
+    global server
+
     config['isManagingServer'] = manageServerInternally
 
     if manageServerInternally:
         print("Starting CoreNLP server...")
+
+        server = CoreNLPServer(
+            os.path.join(STANFORD, "stanford-corenlp-3.9.2.jar"),
+            os.path.join(STANFORD, "stanford-corenlp-3.9.2-models.jar"),
+        )
         server.start()
     else:
         try:
             print("Checking connection to CoreNLP server...")
+
             requests.get(f'{config["coreNLPServerURL"]}/live')
         except BaseException:
             print("Error connecting to CoreNLP instance! Make sure the server is running in the background.")
             print("The relevant command can be found in the README.")
+
             exit(1)
+
 
 def accumulateLongestSequence(acc, token):
     if type(acc) == dict:
@@ -60,6 +66,7 @@ def accumulateLongestSequence(acc, token):
 
     return acc
 
+
 def accumulateString(acc, token):
     if type(acc) == dict:
         sequenceOne = accumulateString("", acc)
@@ -79,9 +86,10 @@ def accumulateString(acc, token):
 
     return acc
 
+
 def extractor(context, contextPOS, contextTokens, contextNamedEntities, contextInformationExtraction,
-              question, questionPOS, questionTokens, questionNamedEntities, questionInformationExtraction,
-              realAnswers=None):
+              questionPOS, questionTokens, questionNamedEntities, questionInformationExtraction,
+              realAnswers = None):
     i = -1
     answers = []
     knowledgeBase = {}
@@ -92,6 +100,8 @@ def extractor(context, contextPOS, contextTokens, contextNamedEntities, contextI
         'numIntentionallyImpossible': 0,
     }
     allPotentialSubjects = []
+    potentialNPSubjects = []
+    relevantSentences = []
 
     print("ANALYSIS OF QUESTION:")
     questionPOS.pretty_print()
@@ -108,27 +118,27 @@ def extractor(context, contextPOS, contextTokens, contextNamedEntities, contextI
     if len(questionInformationExtraction) > 0:
         allPotentialSubjects += list(map(lambda x: x['subject'], questionInformationExtraction))
 
-    potentialSubjects = []
     for t in questionPOS.subtrees(lambda t: t.label() == 'NP'):
-        potentialSubjects.append(t)
+        potentialNPSubjects.append(t)
 
-    if len(potentialSubjects) > 0:
-        for subject in potentialSubjects:
-            startingTokenTree = list(filter(lambda token: next(subject.subtrees(lambda t:
-                 t.label() == token['pos'] and token['originalText'] in list(t.leaves())), False), questionTokens))
-            sequences = reduce(accumulateLongestSequence, startingTokenTree, None)
+    for subject in potentialNPSubjects:
+        startingTokenTree = list(filter(lambda token: next(subject.subtrees(lambda t:
+                                                                            t.label() == token['pos'] and token[
+                                                                                'originalText'] in list(
+                                                                                t.leaves())), False),
+                                        questionTokens))
+        sequences = reduce(accumulateLongestSequence, startingTokenTree, None)
 
-            if sequences:
-                longestSequence = reduce(lambda acc, seq: seq if len(seq) > len(acc) else acc, sequences)
-                longestSequenceAsTokens = reduce(accumulateString, longestSequence, "").strip()
-                allPotentialSubjects += [ longestSequenceAsTokens ]
+        if sequences:
+            longestSequence = reduce(lambda acc, seq: seq if len(seq) > len(acc) else acc, sequences)
+            longestSequenceAsTokens = reduce(accumulateString, longestSequence, "").strip()
+            allPotentialSubjects += [longestSequenceAsTokens]
 
     allPotentialSubjects = list(set(allPotentialSubjects))
     allPotentialSubjects.sort(key=len, reverse=True)
 
     print("ALL POTENTIAL SUBJECTS:")
     print(allPotentialSubjects)
-    potentialSentences = []
 
     for sent in contextPOS:
         i += 1
@@ -143,30 +153,32 @@ def extractor(context, contextPOS, contextTokens, contextNamedEntities, contextI
             subject = item['subject']
 
             if subject in allPotentialSubjects or object in allPotentialSubjects:
-                if fullContext not in potentialSentences:
-                    potentialSentences.append(fullContext)
+                if fullContext not in relevantSentences:
+                    relevantSentences.append(fullContext)
 
             if object in knowledgeBase:
                 knowledgeBase[object].append(item)
             else:
-                knowledgeBase[object] = [ item ]
+                knowledgeBase[object] = [item]
 
         for item in sentNamedEntities:
             text = item['text']
 
             if text in allPotentialSubjects:
-                if fullContext not in potentialSentences:
-                    potentialSentences.append(fullContext)
+                if fullContext not in relevantSentences:
+                    relevantSentences.append(fullContext)
 
         # sent.pretty_print()
 
         for t in sent.subtrees(lambda t: t.label() == 'NP'):
             potentialAnswers.append(t)
-#            print(t.leaves())
+        #            print(t.leaves())
 
         for answer in potentialAnswers:
             startingTokenTree = list(filter(lambda token: next(answer.subtrees(lambda t:
-                t.label() == token['pos'] and token['originalText'] in list(t.leaves())), False), sentTokens))
+                                                                               t.label() == token['pos'] and token[
+                                                                                   'originalText'] in list(t.leaves())),
+                                                               False), sentTokens))
             sequences = reduce(accumulateLongestSequence, startingTokenTree, None)
             longestSequenceAsTokens = None
 
@@ -184,8 +196,8 @@ def extractor(context, contextPOS, contextTokens, contextNamedEntities, contextI
                 index = context.index(longestSequenceAsTokens)
                 answers.append((index, longestSequenceAsTokens))
 
-                if fullContext not in potentialSentences:
-                    potentialSentences.append(fullContext)
+                if fullContext not in relevantSentences:
+                    relevantSentences.append(fullContext)
             except AssertionError as e:
                 if config['debugSentenceElimination']:
                     print(e.args[0])
@@ -200,33 +212,35 @@ def extractor(context, contextPOS, contextTokens, contextNamedEntities, contextI
                 print(context)
                 print("couldn't reconstruct original phrase :(")
 
-
     print("RELEVANT CONTEXT JUDGMENT:")
-    if len(potentialSentences) == 0:
+    if len(relevantSentences) == 0:
         print("No relevant sentences.")
     else:
         print(
-            f"{len(potentialSentences)}/{len(contextPOS)} relevant sentences. The following will be passed as context:")
-        print(*potentialSentences)
+            f"{len(relevantSentences)}/{len(contextPOS)} relevant sentences. The following will be passed as context:")
+        print(*relevantSentences)
 
     flatKnowledgeBase = list(itertools.chain.from_iterable(knowledgeBase.values()))
     simpleFlatKnowledgeBase = list(map(lambda x: (x['subject'].lower(), x['relation'].lower(), x['object'].lower()),
                                        flatKnowledgeBase))
     hasAnswerInKnowledgeBase = any((entity.lower() in x for x in simpleFlatKnowledgeBase)
-        for entity in allPotentialSubjects)
+                                   for entity in allPotentialSubjects)
     answers = list(set(answers))
 
     print("FEASIBILITY ANALYSIS:")
-    if realAnswers != None:
+    if realAnswers is not None:
         if len(realAnswers) > 0:
             if any(realAnswer.lower() in map(lambda x: x[1].lower(), answers) for realAnswer in realAnswers):
                 print("Real answer in answers db.")
+
                 nextGlobalStats['numPossible'] += 1
-            elif any((realAnswer.lower() in x.lower() for x in potentialSentences) for realAnswer in realAnswers):
+            elif any((realAnswer.lower() in x.lower() for x in relevantSentences) for realAnswer in realAnswers):
                 print("Possible with relevant sentence.")
+
                 nextGlobalStats['numPossibleWithRelevantSentences'] += 1
             else:
                 print("Question not answerable with current strategies.")
+
                 nextGlobalStats['numImpossible'] += 1
         else:
             nextGlobalStats['numIntentionallyImpossible'] += 1
@@ -234,17 +248,17 @@ def extractor(context, contextPOS, contextTokens, contextNamedEntities, contextI
     if not hasAnswerInKnowledgeBase:
         print("No subjects found in knowledge base.")
 
-        return (True, 0, 0, nextGlobalStats)
+        return True, 0, 0, nextGlobalStats
 
-    if len(potentialSentences) == 0:
+    if len(relevantSentences) == 0:
         print("No relevant context found.")
 
-        return (True, 0, 0, nextGlobalStats)
+        return True, 0, 0, nextGlobalStats
 
     if len(answers) == 0:
         print("No answers found.")
 
-        return (True, 0, 0, nextGlobalStats)
+        return True, 0, 0, nextGlobalStats
     else:
         print("ALL POTENTIAL ANSWERS:")
         print(answers)
@@ -253,13 +267,12 @@ def extractor(context, contextPOS, contextTokens, contextNamedEntities, contextI
         print("FINAL ANSWER:")
         print(finalAnswer)
 
-        return (False, finalAnswer[0], len(finalAnswer[1]) + finalAnswer[0], nextGlobalStats)
+        return False, finalAnswer[0], len(finalAnswer[1]) + finalAnswer[0], nextGlobalStats
 
-contextCache = {}
 
 # (impossible (bool), starting index, ending index)
 # answer can be provided for obtaining stats about
-def eval(context, question, answers=None):
+def eval(context, question, answers = None):
     questionPOS, questionTokens, questionNamedEntities, questionInformationExtraction = None, None, None, None
 
     # todo: dependency tree stuff
@@ -296,17 +309,16 @@ def eval(context, question, answers=None):
                                                     parsed_sent['tokens'],
                                                     parsed_sent['entitymentions'],
                                                     parsed_sent['openie']), contextParsed['sentences']))
-    contextPOS, contextTokens, contextNamedEntities, contextInformationExtraction =\
-        list(map(lambda x: x[0], contextResponse)), list(map(lambda x: x[1], contextResponse)),\
+    contextPOS, contextTokens, contextNamedEntities, contextInformationExtraction = \
+        list(map(lambda x: x[0], contextResponse)), list(map(lambda x: x[1], contextResponse)), \
         list(map(lambda x: x[2], contextResponse)), list(map(lambda x: x[3], contextResponse))
 
     return extractor(context, contextPOS, contextTokens, contextNamedEntities, contextInformationExtraction,
-                     question, questionPOS, questionTokens, questionNamedEntities, questionInformationExtraction,
+                     questionPOS, questionTokens, questionNamedEntities, questionInformationExtraction,
                      answers)
+
 
 # Teardown code:
 def stop():
     if config['isManagingServer']:
         server.stop()
-
-parser = CoreNLPParser(url=config['coreNLPServerURL'])
